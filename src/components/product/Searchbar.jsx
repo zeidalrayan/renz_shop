765087e0cf49777cd2f7855d1071890f3d837917
alrayanzeid <alrayanzeid@gmail.com>
2024-12-12 10:37:14 +0700

    first commit

--- src/components/product/Searchbar.jsx
+++ src/components/product/Searchbar.jsx
@@ -10,22 +10,7 @@ export default function Searchbar({ handlerSearch }) {
           placeholder="Search..."
           onChange={(e) => handlerSearch(e.target.value)}
         />
-        <div className="absolute right-0 inset-y-0 flex items-center">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="-ml-1 mr-3 h-5 w-5 text-gray-400 hover:text-gray-500"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M6 18L18 6M6 6l12 12"
-            />
-          </svg>
-        </div>
+
         <div className="absolute left-0 inset-y-0 flex items-center">
           <svg
             xmlns="http://www.w3.org/2000/svg"
